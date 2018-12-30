44c9ff1ea9e4e95c7829c73e1698c77244bdb83c
mrajanikanth <mrajanikanth@live>
2018-12-30 20:55:05 +0000

    url to use secureflight

--- cli-workspace/projects/flight-api/src/lib/services/flight.service.ts
+++ cli-workspace/projects/flight-api/src/lib/services/flight.service.ts
@@ -31,7 +31,8 @@ export class FlightService {
     // let url = '/assets/data/data.json';
 
     // For online access
-    let url = [this.baseUrl, 'flight'].join('/');
+    // let url = [this.baseUrl, 'flight'].join('/');
+    let url = this.baseUrl + '/secureflight/byRoute';
 
     if (urgent) {
       url = [this.baseUrl,'error?code=403'].join('/');
